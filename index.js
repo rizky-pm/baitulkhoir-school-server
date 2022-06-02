80b6b26d30c4fe5de71aa017db4d7030a289db59
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2022-06-02 16:43:20 +0700

    First commit | Added basic setup and authentication routes

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import router from './routes/index.js';
+
+dotenv.config();
+const app = express();
+
+const whitelist = ['http://localhost:3000', 'http://localhost:3001'];
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cookieParser());
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(router);
+
+app.listen(5000, () => {
+  console.log('Server running at port 5000');
+});
